817948f5ba779c0a963758b6eb763891eae5ef41
austinChappell <Austin.W.Chappell@gmail.com>
2017-07-14 12:06:27 -0500

    fix equals continue bug

--- main.js
+++ main.js
@@ -26,10 +26,13 @@ let equals = document.querySelector('#equals');
 let dot = document.querySelector('#dot');
 
 function updateAnswer() {
+
   answer.textContent += keyStrk;
+
 }
 
 function setZero() {
+
   total = 0;
   ansStr = '';
   value = [];
@@ -37,17 +40,21 @@ function setZero() {
   numCount = 0;
   operatorCount = 0;
   answer.textContent = ansStr;
+
 }
 
 container.addEventListener('click', function(evt) {
+
   let target = evt.target;
 
   if (target.classList.contains('number')) {
+
     keyStrk = target.innerHTML;
     ansStr += keyStrk;
     updateAnswer();
 
   } else if (target.classList.contains('operator')) {
+
     value[numCount] = Number(ansStr);
     numCount++;
     ansStr = '';
@@ -56,8 +63,11 @@ container.addEventListener('click', function(evt) {
     operatorCount++;
 
   } else if (target.classList.contains('clear')) {
+
     setZero();
+
   } else if (target.classList.contains('equals')) {
+
     value[numCount] = Number(ansStr);
 
     function calculate() {
@@ -68,74 +78,118 @@ container.addEventListener('click', function(evt) {
       let minusIndex = operators.indexOf('-');
 
       function multiply() {
+
         let product = value[timesIndex] * value[timesIndex + 1];
         if (value.length === 2) {
+
           total = product;
           value[timesIndex] = product;
           value.splice(timesIndex + 1, 1);
+
         } else {
+
           value[timesIndex] = product;
           value.splice(timesIndex + 1, 1);
           operators.splice(timesIndex, 1);
+
         }
+
       }
 
       function divide() {
+
         let product = value[divideIndex] / value[divideIndex + 1];
         if (value.length === 2) {
           total = product;
           value[divideIndex] = product;
           value.splice(divideIndex + 1, 1);
+
         } else {
+
           value[divideIndex] = product;
           value.splice(divideIndex + 1, 1);
           operators.splice(divideIndex, 1);
+
         }
+
       }
 
       function add() {
+
         let sum = value[addIndex] + value[addIndex + 1];
         if (value.length === 2) {
+
           total = sum;
           value[addIndex] = sum;
           value.splice(addIndex + 1, 1);
+
         } else {
+
           value[addIndex] = sum;
           value.splice(addIndex + 1, 1);
           operators.splice(addIndex, 1);
+
         }
+
       }
 
       function subtract() {
+
         let sum = value[minusIndex] - value[minusIndex + 1];
         if (value.length === 2) {
+
           total = sum;
           value[minusIndex] = sum;
           value.splice(minusIndex + 1, 1);
+
         } else {
+
           value[minusIndex] = sum;
           value.splice(minusIndex + 1, 1);
           operators.splice(minusIndex, 1);
+
         }
+
       }
 
       if (timesIndex !== -1) {
+
         multiply();
+
       } else if (divideIndex !== -1) {
+
         divide();
+
       } else if (addIndex !== -1) {
+
         add();
+
       } else if (minusIndex !== -1) {
+
         subtract();
+
       }
 
       if (value.length > 1) {
+
         calculate();
+
       } else {
+
         return total;
+
       }
+
     }
+
     calculate();
     answer.textContent = total;
+    value.splice(2, 1);
+    operatorCount = 0;
+    operators = [];
+    ansStr = total;
+    numCount = 0;
+
   }
+
 });
