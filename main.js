1d2730c78d2856e9c625c1227a4a3ede06303187
austinChappell <Austin.W.Chappell@gmail.com>
2017-07-17 15:03:16 -0500

    remove extra variables

--- main.js
+++ main.js
@@ -9,24 +9,7 @@ let allTotalsCount = 0;
 let subTotal;
 
 let container = document.querySelector('.container');
-let one = document.querySelector('#one');
-let two = document.querySelector('#two');
-let three = document.querySelector('#three');
-let four = document.querySelector('#four');
-let five = document.querySelector('#five');
-let six = document.querySelector('#six');
-let seven = document.querySelector('#seven');
-let eight = document.querySelector('#eight');
-let nine = document.querySelector('#nine');
-let zero = document.querySelector('#zero');
-let clear = document.querySelector('#clear');
 let answer = document.querySelector('#answer h2');
-let divide = document.querySelector('#divide');
-let times = document.querySelector('#times');
-let minus = document.querySelector('#minus');
-let plus = document.querySelector('#plus');
-let equals = document.querySelector('#equals');
-let dot = document.querySelector('#dot');
 let audio = document.querySelector('audio');
 
 function sqRoot(index) {
